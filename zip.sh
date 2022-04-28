39caab1d2ba4a82febbfa8c48794096ad4fc8326
nmqamy <92574489+nmqamy@users.noreply.github.com>
2022-04-28 14:48:05 +0430

    Create zip.sh

---
+++ zip.sh
@@ -0,0 +1,25 @@
+zsh: corrupt history file /home/nima/.zsh_history
+┌──(nima㉿kali)-[~]
+└─$ ls   
+Desktop    Downloads  passlist  Public     videos
+Documents  Music      Pictures  Templates  Videos
+                                                                                
+┌──(nima㉿kali)-[~]
+└─$ Downloads
+                                                                                
+┌──(nima㉿kali)-[~/Downloads]
+└─$ ls
+1
+           
+┌──(nima㉿kali)-[~/Downloads]
+└─$ touch 1       
+                                                                                
+┌──(nima㉿kali)-[~/Downloads]
+└─$ zip myZip.zip 1         
+  adding: 1 (deflated 1%)
+                                                                                
+┌──(nima㉿kali)-[~/Downloads]
+└─$ rm 1
+                                                                                
+┌──(nima㉿kali)-[~/Downloads]
+└─$ 
